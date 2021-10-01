1f6294ea34486ff97fce04ac606d10f69ede2ce9
MoaufmKlo <45636897+MoaufmKlo@users.noreply.github.com>
2021-10-01 22:26:28 +0200

    Add JSDoc
    IDE's and text editors such as Visual Studio Code enhance autocompletion and syntax with JSDoc. This pull request adds it to all functions and adds details to any arguments.

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const puns = require("./puns.json");
 
+/**
+ * Get all puns.
+ * @returns {Array} Array of pun objects (pun and punchline)
+ */
 exports.all = () => puns;
 
+/**
+ * Get random pun.
+ * @returns {Object} Pun object (pun and punchline)
+ */
 exports.random = () => {
   const randomId = Math.floor(Math.random() * Math.floor(puns.length))
   puns[randomId].id = randomId
   return puns[randomId]
 };
 
+/**
+ * Get specific pun.
+ * @param {string} [id] Id of pun; returns random pun otherwise
+ * @returns {Object} Pun object (pun and punchline)
+ */
 exports.get = (id) => {
   if (id !== undefined) {
     if (isNaN(id)) {
